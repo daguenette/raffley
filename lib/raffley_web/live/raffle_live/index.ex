2e1066bc6015e79d376b2e478e6e20c987e697cd
daguenette <daguenette.data@gmail.com>
2025-01-03 09:48:04 -0500

    Filter form

--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -5,7 +5,13 @@ defmodule RaffleyWeb.RaffleLive.Index do
   alias RaffleyWeb.CustomComponents
 
   def mount(_params, _session, socket) do
-    socket = stream(socket, :raffles, Raffles.list_raffles())
+
+    socket =
+      socket
+      |> stream(:raffles, Raffles.list_raffles())
+      |> assign(:form, to_form(%{}))
+
+
     {:ok, socket}
   end
 
@@ -17,6 +23,7 @@ defmodule RaffleyWeb.RaffleLive.Index do
         <:details :let={emoji}>To Be Revealed Tomorrow {emoji}</:details>
         <:details>Any guesses?</:details>
       </CustomComponents.banner>
+      <.filter_form form={@form}/>
       <div class="raffles" id="raffles" phx-update="stream">
         <.raffle_card :for={{dom_id, raffle} <- @streams.raffles} raffle={raffle} id={dom_id}/>
       </div>
@@ -43,4 +50,25 @@ defmodule RaffleyWeb.RaffleLive.Index do
     </.link>
     """
   end
+
+  def filter_form(assigns) do
+    ~H"""
+    <.form for={@form}>
+      <.input field={@form[:q]} placeholder="Search..." autocomplete="off" />
+      <.input
+        type="select"
+        field={@form[:status]}
+        prompt="Status"
+        options={[:upcoming, :open, :closed]}
+      />
+      <.input
+        type="select"
+        field={@form[:sort_by]}
+        prompt="Sort By"
+        options={[:prize, :ticket_price]}
+      />
+    </.form>
+    """
+  end
+
 end
