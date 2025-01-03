b94eb78dacd18f51e03dc8e0e5b11e2421245c27
daguenette <daguenette.data@gmail.com>
2025-01-03 10:23:16 -0500

    filtering with dynamic queries

--- lib/raffley/raffles.ex
+++ lib/raffley/raffles.ex
@@ -7,13 +7,13 @@ defmodule Raffley.Raffles do
     Repo.all(Raffle)
   end
 
-  def filter_raffles do
-     Raffle
-     |> where(status: :closed)
-     |> where([r], ilike(r.prize, "%gourmet%"))
-     |> order_by(:prize)
-     |> Repo.all()
-   end
+  def filter_raffles(filter) do
+    Raffle
+    |> with_status(filter["status"])
+    |> search_by(filter["q"])
+    |> sort(filter["sort_by"])
+    |> Repo.all()
+  end
 
   def get_raffle!(id) do
     Repo.get!(Raffle, id)
@@ -26,5 +26,34 @@ defmodule Raffley.Raffles do
     |> order_by(desc: :ticket_price)
     |> limit(3)
     |> Repo.all()
+  end
+
+  # Private functions
+  defp with_status(query, status) when status in ~w(open upcoming closed) do
+    where(query, status: ^status)
+  end
+
+  defp with_status(query, _), do: query
+
+  defp search_by(query, q) when q in ["", nil], do: query
+
+  defp search_by(query, q) do
+    where(query, [r], ilike(r.prize, ^"%#{q}%"))
+  end
+
+  defp sort(query, "prize") do
+    order_by(query, :prize)
+  end
+
+  defp sort(query, "ticket_price_desc") do
+    order_by(query, desc: :ticket_price)
+  end
+
+  defp sort(query, "ticket_price_asc") do
+    order_by(query, asc: :ticket_price)
+  end
+
+  defp sort(query, _) do
+    order_by(query, :inserted_at)
   end
 end
--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -5,12 +5,10 @@ defmodule RaffleyWeb.RaffleLive.Index do
   alias RaffleyWeb.CustomComponents
 
   def mount(_params, _session, socket) do
-
     socket =
       socket
       |> stream(:raffles, Raffles.list_raffles())
       |> assign(:form, to_form(%{}))
-
 
     {:ok, socket}
   end
@@ -23,9 +21,9 @@ defmodule RaffleyWeb.RaffleLive.Index do
         <:details :let={emoji}>To Be Revealed Tomorrow {emoji}</:details>
         <:details>Any guesses?</:details>
       </CustomComponents.banner>
-      <.filter_form form={@form}/>
+      <.filter_form form={@form} />
       <div class="raffles" id="raffles" phx-update="stream">
-        <.raffle_card :for={{dom_id, raffle} <- @streams.raffles} raffle={raffle} id={dom_id}/>
+        <.raffle_card :for={{dom_id, raffle} <- @streams.raffles} raffle={raffle} id={dom_id} />
       </div>
     </div>
     """
@@ -53,8 +51,8 @@ defmodule RaffleyWeb.RaffleLive.Index do
 
   def filter_form(assigns) do
     ~H"""
-    <.form for={@form}>
-      <.input field={@form[:q]} placeholder="Search..." autocomplete="off" />
+    <.form for={@form} id="filter-form" phx-change="filter">
+      <.input field={@form[:q]} placeholder="Search..." autocomplete="off" phx-debounce="500" />
       <.input
         type="select"
         field={@form[:status]}
@@ -65,10 +63,22 @@ defmodule RaffleyWeb.RaffleLive.Index do
         type="select"
         field={@form[:sort_by]}
         prompt="Sort By"
-        options={[:prize, :ticket_price]}
+        options={[
+          Prize: "prize",
+          "Price: High to Low": "ticket_price_desc",
+          "Price: Low to High": "ticket_price_asc"
+        ]}
       />
     </.form>
     """
   end
 
+  def handle_event("filter", params, socket) do
+    socket =
+      socket
+      |> assign(:form, to_form(params))
+      |> stream(:raffles, Raffles.filter_raffles(params), reset: true)
+
+    {:noreply, socket}
+  end
 end
